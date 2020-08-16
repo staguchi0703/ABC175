f64d1c004a823856ad7a465f0fdf7a276894bc43
Sho Taguchi <s.taguchi703@gmail.com>
2020-08-16 09:57:27 +0900

    あとちょっと　解の変数設定

--- D/resolve.py
+++ D/resolve.py
@@ -6,52 +6,29 @@ def resolve():
     Ps = [int(item)-1 for item in input().split()]
     Cs = [int(item) for item in input().split()]
 
-    loops = [[0 for _ in range(N)] for _ in range(N)]
+    ans = -100**5
     for i in range(N):
         next_node = i
-        loops[i][i] = 1
-        while True:
-            next_node = Ps[next_node]
-            if loops[i][next_node] != 1:
-                loops[i][next_node] = 1
-            else:
-                break
-
-    prio_num = [0 for _ in range(N)]
-    for i in range(N):
-        prio_num[i] = sum(loops[i])
-
-    # print(prio_num)
-    res_list = [0 for _ in range(N)]
-    for i in range(N):
-        num = K % prio_num[i]
-        if num == 0:
-            num = K
-
-        prio = prio_num[i]
-        cnt = 0
         temp_num = 0
-        prio_num1 = 0
-
-        next_node = i
-        temp_max = -10000000000000000
-        while cnt < prio:
+        temp_res = -100**5
+        cnt = 0
+        looped_flag = True
+        while Ps[next_node] != i:
+            if cnt >= K:
+                looped_flag = False
+                break
             next_node = Ps[next_node]
-            prio_num1 += Cs[next_node]
-            
-            if cnt < num:
-                temp_num += Cs[next_node]
-                temp_max = max(temp_max, temp_num)
-                # print(i, temp_num, temp_max)
+            temp_num += Cs[next_node]
+            ans = max(ans, temp_num)
             cnt += 1
 
-        if prio_num1 > 0:
-            res_list[i] = temp_max + prio_num1 * (K //prio)
-        else:
-            res_list[i] = temp_max
+        if temp_num > 0 and looped_flag:
+            ans += (K // cnt) * temp_num
 
-    # print(res_list)
-    print(max(res_list))
+        ans = max(ans, )
+
+
+
 
 if __name__ == "__main__":
     resolve()
